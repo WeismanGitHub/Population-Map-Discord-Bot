c0a773c55ef9160f23dba671a161fe9ed6affadb
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-04 16:53:57 -0700

    added not found error class

--- src/server/errors/index.ts
+++ src/server/errors/index.ts
@@ -1,7 +1,9 @@
 import BadRequestError from "./bad-request";
 import CustomError from "./custom-error";
+import NotFoundError from "./not-found";
 
 export {
     BadRequestError,
     CustomError,
+    NotFoundError
 }
