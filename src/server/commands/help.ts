8811511a4572ee211ef5d2b8a9d108d99f6fa4d8
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-05 12:06:28 -0700

    created

---
+++ src/server/commands/help.ts
@@ -0,0 +1,41 @@
+import config from '../config'
+import {
+	SlashCommandBuilder,
+	CommandInteraction,
+	EmbedBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+} from 'discord.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription("Information about this bot.")
+	,
+	async execute(interaction: CommandInteraction): Promise<void> {
+		const embed: EmbedBuilder = new EmbedBuilder()
+		.setColor('#FF7B00') // orange
+		.setDescription("Generate a population map based off of server member's self reported locations.")
+		.addFields({ name: 'Contact the Creator:', value: `<@${config.mainAccountID}>` })
+		.setImage('../../../population-map-example.png')
+	
+		const row = new ActionRowBuilder<ButtonBuilder>()
+		.addComponents([
+			new ButtonBuilder()
+			.setLabel('Github')
+			.setURL(config.githubURL)
+			.setStyle(ButtonStyle.Link),
+			new ButtonBuilder()
+			.setLabel('Buy Me a Coffee')
+			.setURL(config.buyMeACoffeeURL)
+			.setStyle(ButtonStyle.Link),
+		])
+
+		interaction.reply({
+			embeds: [embed],
+			components: [row],
+			ephemeral: true
+		})
+	}
+}
