1a204358e0caa48f5b965c2b3ad82551093b8868
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-07 11:54:32 -0700

    updated index.js override

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     paths: function (paths, env) {
         paths.appHtml = path.resolve(__dirname, 'src/client/public/index.html')
-        paths.appIndexJs = path.resolve(__dirname, 'src/client/src/index.js')
+        paths.appIndexJs = path.resolve(__dirname, 'dist/client/src/index.js')
         paths.appPublic = path.resolve(__dirname, 'src/client/public')
         paths.appBuild = path.resolve(__dirname, 'dist/client/build')
         paths.appSrc = path.resolve(__dirname, 'src/client/src')
