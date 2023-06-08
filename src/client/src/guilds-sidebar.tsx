b0da80b5b1b246efa3983ec2684f1e4e0392c39c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-07 19:32:46 -0700

    buildable

--- src/client/src/guilds-sidebar.tsx
+++ src/client/src/guilds-sidebar.tsx
@@ -1,25 +1,24 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import axios, * as others from 'axios'
+// import axios, * as others from 'axios'
 import React from 'react'
 
 export default function GuildsSidebar() {
-    const [searchParams, setSearchParams] = useSearchParams();
     const [guilds, setGuilds] = useState([])
+    setGuilds([])
 
     useEffect(() => {
     }, [])
 
     return <div className='guilds'>
         { guilds.map(guild => <div>
-            <img
+            {/* <img
                 src={guild.iconURL}
                 alt="server icon"
                 width = "60"
                 height = "60"
                 title = {guild.name}
                 onClick={() => {}}>
-            </img>
+            </img> */}
         </div>)}
     </div>
 }
