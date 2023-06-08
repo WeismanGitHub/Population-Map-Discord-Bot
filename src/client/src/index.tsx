d2e1751bee16ba4ad4084964453d886de09b365f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-07 19:23:19 -0700

    changed routes stuff

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import React from 'react';
 // import './css/index.css';
 
+import Guilds from './guilds';
 import Home from './home';
 
 const router = createBrowserRouter([
     { path: '/', element: <Home/> },
+    { path: '/guilds(/:guildID)', element: <Guilds/> },
 ]);
 
 createRoot(document.getElementById("root")!).render(<RouterProvider router={router}/>);
