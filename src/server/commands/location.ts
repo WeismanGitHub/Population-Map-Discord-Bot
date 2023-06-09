39a687e0ea677110f154defc30978969356a0c1e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-09 15:31:20 -0700

    created location command

---
+++ src/server/commands/location.ts
@@ -0,0 +1,50 @@
+import {
+    SlashCommandBuilder,
+    CommandInteraction,
+    StringSelectMenuBuilder,
+    StringSelectMenuOptionBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} from 'discord.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('location')
+		.setDescription("Set your country and optionally your subdivision/state/region.")
+	,
+	async execute(interaction: CommandInteraction): Promise<void> {
+        const menuRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(new StringSelectMenuBuilder()
+        .setCustomId(`test`)
+			.setPlaceholder('Make a selection!')
+			.addOptions(
+                // @ts-ignore
+                interaction.client.countries.slice(0, 25).map(country => 
+                    new StringSelectMenuOptionBuilder()
+                        .setLabel(country.name)
+                        .setValue(country.name)
+                )
+            )
+        )
+
+        const buttonsRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+                .setLabel('⏪')
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId('0')
+                .setDisabled(true),
+            new ButtonBuilder()
+                .setLabel('⏩')
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId(JSON.stringify({
+                    type: 'location',
+                    data: { page: 1 }
+                }))
+        )
+
+        interaction.reply({
+            ephemeral: true,
+            components: [menuRow, buttonsRow]
+        })
+	}
+}
