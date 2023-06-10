aa795ccdcf5b18f593ae3e0b8c27d7e79174f98b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-09 20:16:56 -0700

    completed

---
+++ src/server/events/location/subdivision-menu.ts
@@ -0,0 +1,29 @@
+import { infoEmbed } from "../../utils/embeds";
+import User from "../../db/models/user";
+import {
+    Events,
+    StringSelectMenuInteraction
+} from "discord.js"
+import { InternalServerError } from "../../errors";
+
+export default {
+	name: Events.InteractionCreate,
+	once: false,
+    execute: async (interaction: StringSelectMenuInteraction) => {
+        if (!interaction.isStringSelectMenu()) return;
+
+        const { type }: CustomID<{}> = JSON.parse(interaction.customId)
+
+        if (type !== 'location-subdivision') return
+
+        const subdivisionCode = interaction.values[0]
+
+        User.upsert({ discordID: interaction.user.id, subdivisionCode })
+        .catch(err => { throw new InternalServerError('Could not save your subdivision.') })
+
+        interaction.update({
+            embeds: [infoEmbed('Make sure to choose your settings with `/settings`!')],
+            components: []
+        })
+    }
+}
