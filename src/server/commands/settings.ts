e6e7f918f7de323133df1540a9e4f58e37253e02
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-09 18:28:34 -0700

    just boilerplate

--- src/server/commands/settings.ts
+++ src/server/commands/settings.ts
@@ -1,27 +1,13 @@
-import { SlashCommandBuilder, CommandInteraction } from 'discord.js'
-import User from '../db/models/user'
+import {
+    SlashCommandBuilder,
+    CommandInteraction,
+} from 'discord.js'
 
 export default {
 	data: new SlashCommandBuilder()
 		.setName('settings')
-		.setDescription("Set your settings.")
-        .addStringOption(option => option
-            .setName('country')
-            .setDescription("Set what country you live in.")
-            // .autocomplete()
-        )
-        .addStringOption(option => option
-            .setName('visibility')
-            .setDescription("Automatically allow maps to be generated using you everywhere.")
-            .addChoices(
-                { name: 'allow all guilds', value: 'true' },
-                { name: 'manually decide which guilds', value: 'false' },
-            )
-        )
+		.setDescription("Choose your settings.")
 	,
 	async execute(interaction: CommandInteraction): Promise<void> {
-        const user = await User.create({ discordID: interaction.user.id })
-        console.log(user)
-        interaction.reply('sdfs')
 	}
 }
