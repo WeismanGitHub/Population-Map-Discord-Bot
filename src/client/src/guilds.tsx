054ded50191dfb001af60fcf9b92407f0b207f92
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-11 16:34:20 -0700

    i need to commit before switching to a new branch

--- src/client/src/guilds.tsx
+++ src/client/src/guilds.tsx
@@ -1,36 +1,53 @@
+// @ts-nocheck
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import Plot from 'react-plotly.js';
-import React from 'react'
+import ky from 'ky';
 
 export default function Guilds() {
+    const [geojson, setGeojson] = useState(undefined)
     const { guildID } = useParams()
+    
+    useEffect(() => {
+        (async () => {
+            const res = await ky.get('/api/v1/geojson/countries/US').json()
+            console.log(res)
+            setGeojson(res)
+        })()
+    }, [])
 
-    return (<>
-        <Plot
-            data={[
-                {
-                    type: 'choropleth',
-                    locationmode: 'USA-states',
-                    locations: ['CA', 'OR', 'NY'],
-                    z: [1, 2, 10],
-                    text: ['California', 'Oregon', 'New York'],
-                    colorscale: [
-                        [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
-                        [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
-                        [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
-                    ],
-                    colorbar: {
-                        title: 'Users',
-                        thickness: 2,
-                    }
-                },
-            ]}
-            layout={{
-                title: `Population Density of ${guildID}`,
-                geo: { scope: 'usa', showlakes: false },
-                height: 500,
-                width: 1000
-            }}
-        />
-    </>)
+    if (!geojson) {
+        return <h1>loading map...</h1>
+    } else {
+        return (<>
+            <Plot
+                data={[
+                    {
+                        type: 'choropleth',
+                        locationmode: 'geojson-id',
+                        locations: ['CA', 'OR', 'NY'],
+                        z: [1, 2, 10],
+                        text: ['California', 'Oregon', 'New York'],
+                        colorscale: [
+                            [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
+                            [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
+                            [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
+                        ],
+                        colorbar: {
+                            title: 'Users',
+                            thickness: 2,
+                        },
+                        lon: geojson,
+                        lat: geojson,
+                    },
+                ]}
+                layout={{
+                    title: `Population Density of ${guildID}`,
+                    // geo: { scope: 'usa', showlakes: false },
+                    height: 500,
+                    width: 1000
+                }}
+            />
+        </>)
+    }
 }
