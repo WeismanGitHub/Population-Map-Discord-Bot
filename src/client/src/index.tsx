fa5281a5b8bdb84a713d58a8c3a3d570bb89e326
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-10 18:43:28 -0700

    added a discord auth route

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import React from 'react';
 // import './css/index.css';
 
+import DiscordAuth from "./discord-oauth";
 import Guilds from './guilds';
 import Home from './home';
 
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
             element: <Guilds/>
         }]
     },
+    { path: '/auth/discord', element: <DiscordAuth/> },
 ]);
 
 createRoot(document.getElementById("root")!).render(<RouterProvider router={router}/>);
