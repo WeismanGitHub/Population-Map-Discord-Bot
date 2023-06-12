79a92352d63ca457d31763f7d6833a517725d197
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-11 21:40:48 -0700

    finished mapVisibility

--- src/server/events/help/owners-button.ts
+++ src/server/events/help/owners-button.ts
@@ -13,7 +13,7 @@ export default {
 
         const serverOwnerDocs = "Set your server's settings with `/server-settings`. Running `/server-settings` without any options selected just shows the server settings."
 
-        const mapVisibility = "map visibility settings."
+        const mapVisibility = "You can change the visibility of your server's map with the `visibility` option in `/server-settings`. Setting `visibility` to `public` allows anyone with a link to view your server's map. Setting it to `member-restricted` allows only server members to view the map. Choosing the `map-role-restricted` option restricts the map to members with a specific role. You need to set the map role with the `map-role` option."
 
         const adminRole = "admin role settings."
 
