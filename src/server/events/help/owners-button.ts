23836b4e0af7824c0bec555d8fca65f2cd1f8b35
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-11 22:16:23 -0700

    finished up serverRoles section

--- src/server/events/help/owners-button.ts
+++ src/server/events/help/owners-button.ts
@@ -11,15 +11,15 @@ export default {
 
         if (type !== 'help-owners') return
 
-        const serverOwnerDocs = "Set your server's settings with `/server-settings`. Running `/server-settings` without any options selected just shows the server settings."
+        const serverOwnerDocs = "Set a server's settings with `/server-settings`. Running `/server-settings` without any options selected just shows the server settings."
 
-        const mapVisibility = "You can change the visibility of your server's map with the `visibility` option in `/server-settings`. Setting `visibility` to `public` allows anyone with a link to view your server's map. Setting it to `member-restricted` allows only server members to view the map. Choosing the `map-role-restricted` option restricts the map to members with a specific role. You need to set the map role with the `map-role` option."
-
-        const adminRole = "admin role settings."
+        const serverRoles = "The admin role permits a server owner to allow members with a certain role to be able to change any server setting. Select a role to become the admin role with the `admin-role` option.\n\nThe map role allows you to restrict map access to members with a role. You need to set the map role with the `map-role` option."
+        
+        const mapVisibility = "You can change the visibility of your server's map with the `visibility` option in `/server-settings`. Setting `visibility` to `public` allows anyone with a link to view your server's map. Setting it to `member-restricted` allows only server members to view the map. Choosing `map-role-restricted` restricts the map to members with the map role. The `admin-role-restricted` choice limits access to only members with the admin role."
 
         interaction.reply({
-            embeds: [infoEmbed(null, `
-            # Server Owner Docs:\n${serverOwnerDocs}\n## Map Visibility:\n${mapVisibility}\n## Admin Role:\n${adminRole}
+            embeds: [infoEmbed(null,
+                `# Server Owner Docs:\n${serverOwnerDocs}\n## Server Roles:\n${serverRoles}\n## Map Visibility:\n${mapVisibility}
             `)],
             ephemeral: true
         })
