fb1ae438ff14655800d7221a74aedb1e7bf5cadd
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-11 20:14:17 -0700

    made progress

--- src/server/events/help/owners-button.ts
+++ src/server/events/help/owners-button.ts
@@ -11,8 +11,16 @@ export default {
 
         if (type !== 'help-owners') return
 
+        const serverOwnerDocs = "Set your server's settings with `/server-settings`. Running `/server-settings` without any options selected just shows the server settings."
+
+        const mapVisibility = "map visibility settings."
+
+        const adminRole = "admin role settings."
+
         interaction.reply({
-            embeds: [infoEmbed('owners information')],
+            embeds: [infoEmbed(null, `
+            # Server Owner Docs:\n${serverOwnerDocs}\n## Map Visibility:\n${mapVisibility}\n## Admin Role:\n${adminRole}
+            `)],
             ephemeral: true
         })
     }
