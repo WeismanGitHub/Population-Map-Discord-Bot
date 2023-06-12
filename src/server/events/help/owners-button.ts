3c9de6ae2a5821d578ee046d0bb8fdd7d4356dd0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-11 22:30:05 -0700

    rephrased some stuff

--- src/server/events/help/owners-button.ts
+++ src/server/events/help/owners-button.ts
@@ -13,13 +13,15 @@ export default {
 
         const serverOwnerDocs = "Set a server's settings with `/server-settings`. Running `/server-settings` without any options selected just shows the server settings."
 
-        const serverRoles = "The admin role permits a server owner to allow members with a certain role to be able to change any server setting. Select a role to become the admin role with the `admin-role` option.\n\nThe map role allows you to restrict map access to members with a role. You need to set the map role with the `map-role` option."
+        const serverRoles = "The admin role permits a server owner to authorizes members with a certain role to be able to change any server setting. Select a role to become the admin role with the `admin-role` option. Only the server owner is allowed to change the admin role.\n\nThe map role allows admins/owners to restrict map access to members with a role. Set the map role with the `map-role` option."
         
-        const mapVisibility = "You can change the visibility of your server's map with the `visibility` option in `/server-settings`. Setting `visibility` to `public` allows anyone with a link to view your server's map. Setting it to `member-restricted` allows only server members to view the map. Choosing `map-role-restricted` restricts the map to members with the map role. The `admin-role-restricted` choice limits access to only members with the admin role."
+        const mapVisibility = "The `visibility` option allows admins/owners to change who can view the server map. Setting `visibility` to `public` allows anyone with a link to view the server map. Setting it to `member-restricted` allows only server members to view the map. Choosing `map-role-restricted` restricts the map to the server owner and members with the map role. The `admin-role-restricted` choice limits access to only the server owner and members with the admin role. Selecting `invisible` blocks the map to everyone, including server owners and admins."
+
+        const defaults = "settings defaults"
 
         interaction.reply({
             embeds: [infoEmbed(null,
-                `# Server Owner Docs:\n${serverOwnerDocs}\n## Server Roles:\n${serverRoles}\n## Map Visibility:\n${mapVisibility}
+            `# Server Owner Docs\n${serverOwnerDocs}\n## Server Roles\n${serverRoles}\n## Map Visibility\n${mapVisibility}\n## Defaults\n${defaults}
             `)],
             ephemeral: true
         })
