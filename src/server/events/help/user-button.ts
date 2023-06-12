1cadaaa2ce054254f72b605bdeaaa1ce72c5087a
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-12 00:00:12 -0700

    rephrased something

--- src/server/events/help/user-button.ts
+++ src/server/events/help/user-button.ts
@@ -13,7 +13,7 @@ export default {
 
         const userDocs = "Countries and subdivisions are from [ISO 3166](https://www.iso.org/iso-3166-country-codes.html). You can delete your data with `/user-delete`."
 
-        const location = "Save your location with the `/location` command. You must choose your country and optionally your subdivision (state, region, prefecture, etc). Your location is the same across all servers, and it isn't shared with any servers automatically. You need to use `/add-location` in a server to add your location to the server's map. Use `/remove-location` anywhere to remove your location from any server's map."
+        const location = "Save your location with the `/location` command. You must choose your country and optionally your subdivision (state, region, prefecture, etc). Your location is the same across all servers, and isn't shared with any servers automatically. You need to manually use `/add-location` in a server to add your location to the server's map. Use `/remove-location` anywhere to remove your location from any server's map."
 
         interaction.reply({
             embeds: [infoEmbed(null,
