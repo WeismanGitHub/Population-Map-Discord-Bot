5540d851f1927f9b73834ef3faec622a1d6154d1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-11 23:05:40 -0700

    finished userDocs

--- src/server/events/help/user-button.ts
+++ src/server/events/help/user-button.ts
@@ -11,11 +11,13 @@ export default {
 
         if (type !== 'help-users') return
 
-        const userDocs = "Set your location with the `/location` command. You must choose your country and optionally your subdivision (state, region, prefecture, etc). The countries and subdivisions are from [ISO 3166](https://www.iso.org/iso-3166-country-codes.html). You can change your settings with `/user-settings`."
+        const userDocs = "Set your location with the `/location` command. You must choose your country and optionally your subdivision (state, region, prefecture, etc). Your location is universal and the same across all servers. To add your location to a server's map, you need to use the `/add-location` command. All countries and subdivisions are from [ISO 3166](https://www.iso.org/iso-3166-country-codes.html). You can change your settings with `/user-settings`."
+
+        const settings = ""
 
         interaction.reply({
             embeds: [infoEmbed(null,
-            `# User Docs\n${userDocs}
+            `# User Docs\n${userDocs}\nSettings\n${settings}
             `)],
             ephemeral: true
         })
