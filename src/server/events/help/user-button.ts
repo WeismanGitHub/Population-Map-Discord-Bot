5e2ed961151205ea05fe5a1263df21575eda6adc
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-12 20:24:50 -0700

    updated

--- src/server/events/help/user-button.ts
+++ src/server/events/help/user-button.ts
@@ -11,7 +11,7 @@ export default {
 
         if (type !== 'help-users') return
 
-        const userDocs = "Countries and subdivisions are from [ISO 3166](https://www.iso.org/iso-3166-country-codes.html). You can delete your data with `/user-delete`."
+        const userDocs = "Countries and subdivisions are from [ISO 3166](https://www.iso.org/iso-3166-country-codes.html). You can delete your data with `/user-delete` and view your location with `/user-settings`."
 
         const location = "Save your location with the `/location` command. You must choose your country and optionally your subdivision (state, region, prefecture, etc). Your location is the same across all servers, and isn't shared with any servers automatically. You need to manually use `/add-location` in a server to add your location to the server's map. Use `/remove-location` anywhere to remove your location from any map."
 
