5d6f2a73bdfd65778c6e042900a8b73566338ada
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-12 20:28:31 -0700

    created user settings command

---
+++ src/server/commands/user-settings.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js'
+import { infoEmbed } from '../utils/embeds'
+import { User } from '../db/models'
+
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('user-settings')
+		.setDescription("View your location.")
+	,
+	async execute(interaction: ChatInputCommandInteraction) {
+        const user = await User.findOne({ where: { discordID: interaction.user.id } })
+
+        if (!user) {
+            return interaction.reply({
+                ephemeral: true,
+                embeds: [infoEmbed('You need to first set your location with `/location`.', 'Please read the documentation with `/help`.')]
+            })
+        }
+        
+        interaction.reply({
+            ephemeral: true,
+            embeds: [infoEmbed('Your location:', `Country: ${user?.countryCode}\nSubdivision: ${user.subdivisionCode}`)]
+        })
+	}
+}
