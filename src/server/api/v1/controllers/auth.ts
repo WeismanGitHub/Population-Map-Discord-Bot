819015904856d5acdd569de16cd999c6b5e272fe
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-13 23:14:53 -0700

    made progress

--- src/server/api/v1/controllers/auth.ts
+++ src/server/api/v1/controllers/auth.ts
@@ -1,65 +1,56 @@
 import { BadRequestError, InternalServerError } from '../../../errors';
-const DiscordOauth2 = require("discord-oauth2");
-// import { UserModel } from '../../../db/models';
 import { Request, Response } from 'express';
+import DiscordOauth2 from 'discord-oauth2'
+import { User } from '../../../db/models'
 import config from '../../../config';
 require('express-async-errors');
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
 async function discordLogin(req: Request, res: Response): Promise<void> {
     const oauth = new DiscordOauth2();
-    let userID: string | undefined;
     const { code } = req.body
 
     if (!code) {
         throw new BadRequestError('Missing Code')
     }
 
-    try {
-        const token: string = (await oauth.tokenRequest({
-            clientId: config.botID,
-            clientSecret: config.botSecret,
-        
-            code: code,
-            scope: 'identify guilds',
-            grantType: "authorization_code",
-            redirectUri: config.redirectURI,
-        })).access_token
+    const token = (await oauth.tokenRequest({
+        clientId: config.botID,
+        clientSecret: config.botSecret,
+    
+        code: code,
+        scope: 'identify guilds',
+        grantType: "authorization_code",
+        redirectUri: config.redirectURI,
+    })).access_token
 
-        userID = (await oauth.getUser(token)).id
-    } catch(err) {
-        throw new InternalServerError('Could not get user ID.')
-    }
+    const userID = (await oauth.getUser(token)
+    .catch(err => { throw new InternalServerError('Could not get user data.') })).id
 
-    if (!userID) {
-        throw new InternalServerError('Could not get user ID.')
-    }
+    const guilds = (await oauth.getUserGuilds(token)
+    .catch(err => { throw new InternalServerError('Could not get user data.') }))
 
-    const user = await UserModel.findOne({ _id: userID }).lean()
-    .catch((err: Error) => {
-        throw new InternalServerError("Error finding user.")
-    })
+    const user = await User.findOne({ where: { discordID: userID } })
+    .catch(err => { throw new InternalServerError("Error finding user.") })
 
-    if (!user) {
-        throw new UnauthorizedError("You need to register first.")
-    }
+    console.log(userID, guilds, user)
 
-    const userJWT = jwt.sign(
-        { _id: userID, role: user.role },
-        config.jwtSecret,
-        { expiresIn: '14d' },
-    )
+    // const userJWT = jwt.sign(
+    //     { _id: userID, role: user.role },
+    //     config.jwtSecret,
+    //     { expiresIn: '14d' },
+    // )
 
-    const expiration = new Date(Date.now() + (3600000 * 24 * 14)) // 14 days
+    // const expiration = new Date(Date.now() + (3600000 * 24 * 14)) // 14 days
 
-	res.status(200)
-	.cookie('user', userJWT, {
-		httpOnly: true,
-		secure: true,
-		sameSite: 'strict',
-		expires: expiration
-	})
-	.json({ role: user.role }).end()
+	// res.status(200)
+	// .cookie('user', userJWT, {
+	// 	httpOnly: true,
+	// 	secure: true,
+	// 	sameSite: 'strict',
+	// 	expires: expiration
+	// })
+	// .json({ role: user.role }).end()
 }
 
 function logout(req: Request, res: Response): void {
