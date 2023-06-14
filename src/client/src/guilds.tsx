5ab22325d124e6908be3211117f372246f2c7f73
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-13 20:56:52 -0700

    fixed

--- src/client/src/guilds.tsx
+++ src/client/src/guilds.tsx
@@ -1,4 +1,4 @@
-import worldMap from "@highcharts/map-collection/custom/usa-and-canada.geo.json";
+import worldMap from "@highcharts/map-collection/custom/world.geo.json";
 import HighchartsReact from 'highcharts-react-official'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
