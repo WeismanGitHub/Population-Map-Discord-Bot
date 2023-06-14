75e0996d9986d50e6073aa02b1f7158328433b66
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-14 01:19:54 -0700

    added addLocationOnJoin option

--- src/server/commands/user-settings.ts
+++ src/server/commands/user-settings.ts
@@ -7,17 +7,27 @@ import { User } from '../db/models'
 export default {
 	data: new SlashCommandBuilder()
 		.setName('user-settings')
-		.setDescription("View your location.")
+		.setDescription("View your location and change your settings.")
+        .addBooleanOption(option => option
+            .setName('add-location-on-join')
+            .setDescription("Automatically add your location to a server's map when you join. Off by default.")
+        )
 	,
 	async execute(interaction: ChatInputCommandInteraction) {
         const user = await User.findOne({ where: { discordID: interaction.user.id } })
         const client = interaction.client as CustomClient
+        const addLocationOnJoin = interaction.options.getBoolean('add-location-on-join')
 
         if (!user) {
             return interaction.reply({
                 ephemeral: true,
                 embeds: [infoEmbed('You need to first set your location with `/location`.', 'Please read the documentation with `/help`.')]
             })
+        }
+
+        if (addLocationOnJoin !== null) {
+            user.update({ addLocationOnJoin})
+            .catch(err => { throw new InternalServerError('Could not save `add-location-on-join` setting.') })
         }
         
         const country = client.countries.find((country) => country.code === user.countryCode)
@@ -28,10 +38,11 @@ export default {
 
         interaction.reply({
             ephemeral: true,
-            embeds: [infoEmbed('Your location:',
-            `Country: ${country.name}\n
-            ${user.subdivisionCode ? `Subdivision: ${country.sub.find(sub => sub.code === user.subdivisionCode)?.name}` : ''}`
-            )]
+            embeds: [infoEmbed('Your Data:',`
+            Country: ${country.name}
+            ${user.subdivisionCode ? `Subdivision: ${country.sub.find(sub => sub.code === user.subdivisionCode)?.name}` : ''}\n
+            \`add-location-on-join\`: \`${user.addLocationOnJoin}\`
+            `)]
         })
 	}
 }
