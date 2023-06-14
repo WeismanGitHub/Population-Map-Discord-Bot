73f33d005552e7171b7ec502bf5e7696059a33cc
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-14 00:44:48 -0700

    added a comment

--- src/server/events/guild-member-remove.ts
+++ src/server/events/guild-member-remove.ts
@@ -4,5 +4,6 @@ export default {
 	name: Events.GuildMemberRemove,
 	once: false,
 	execute(member: GuildMember) {
+		// if the user has location in server map then remove it from server map.
 	},
 };
