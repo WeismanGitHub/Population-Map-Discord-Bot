33badce6df3b753b10939cedb6ec7e50ca5f2250
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-15 21:43:07 -0700

    create guild and guildmap rows on server join

--- src/server/events/guild-create.ts
+++ src/server/events/guild-create.ts
@@ -5,5 +5,10 @@ export default {
 	name: Events.GuildCreate,
 	once: false,
 	async execute(guild: Discord.Guild) {
+		const res = await Guild.upsert({ ID: guild.id })
+
+		if (!res[1]) {
+			await GuildMap.upsert({ ID: guild.id })
+		}
 	},
 };
