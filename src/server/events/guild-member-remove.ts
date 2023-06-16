cd1c245a85bf88ebad59a307bcbeb76ff1650402
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-15 21:40:18 -0700

    return if member is bot

--- src/server/events/guild-member-remove.ts
+++ src/server/events/guild-member-remove.ts
@@ -4,6 +4,8 @@ export default {
 	name: Events.GuildMemberRemove,
 	once: false,
 	async execute(member: GuildMember) {
+		if (!member.user.bot) return
+
 		console.log(member)
 		// if the user has location in server map then remove it from server map.
 	},
