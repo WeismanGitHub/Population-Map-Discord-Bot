4e64caf0e0f4f7af9cd53725493fc6e69c35b1b5
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-15 21:20:15 -0700

    made progress

--- src/server/api/v1/controllers/map.ts
+++ src/server/api/v1/controllers/map.ts
@@ -1,10 +1,20 @@
-import { BadRequestError } from '../../../errors';
+import { ForbiddenError, NotFoundError } from '../../../errors';
 import { Request, Response } from 'express';
+import { Guild } from '../../../db/models';
 require('express-async-errors')
 
 async function getGuildMap(req: Request, res: Response): Promise<void> {
     const guildID = req.params.guildID
 
+    const visibility = (await Guild.findOne({ where: { ID: guildID }, attributes: ['visibility'] }))?.visibility
+
+    if (!visibility) {
+        throw new NotFoundError('Server is not in database.')
+    }
+
+    if (visibility === 'invisibile') {
+        throw new ForbiddenError('Server visibility is invisible.')
+    }
 
     res.status(200).json()
 }
