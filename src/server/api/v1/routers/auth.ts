a76cc05d901f1acc91f26e5817159101c9e00393
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-15 19:00:27 -0700

    added session middleware

--- src/server/api/v1/routers/auth.ts
+++ src/server/api/v1/routers/auth.ts
@@ -1,9 +1,10 @@
 import { discordOAuth2, logout } from '../controllers/auth'
+import { session } from '../middleware';
 import { Router } from 'express';
 
 const authRouter: Router = Router();
 
-authRouter.post('/logout', logout)
-authRouter.post('/discord/oauth2', discordOAuth2)
+authRouter.post('/logout', session, logout)
+authRouter.post('/discord/oauth2', session, discordOAuth2)
 
 export default authRouter
