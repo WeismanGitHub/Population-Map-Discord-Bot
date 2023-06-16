61965547ee92c8534df0fc4f2dadf32af724c07e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-15 21:04:45 -0700

    added session middleware

--- src/server/api/v1/routers/map.ts
+++ src/server/api/v1/routers/map.ts
@@ -1,8 +1,9 @@
 import { getGuildMap } from '../controllers/map'
+import { session } from '../middleware';
 import { Router } from 'express';
 
 const mapRouter: Router = Router();
 
-mapRouter.get('/guildID', getGuildMap)
+mapRouter.get('/guildID', session, getGuildMap)
 
 export default mapRouter
