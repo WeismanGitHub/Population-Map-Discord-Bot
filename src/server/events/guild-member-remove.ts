b80bb654f670b36b0d228f65c4084b7d05dd0479
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-15 21:38:42 -0700

    made execute async

--- src/server/events/guild-member-remove.ts
+++ src/server/events/guild-member-remove.ts
@@ -3,7 +3,8 @@ import { Events, GuildMember } from 'discord.js'
 export default {
 	name: Events.GuildMemberRemove,
 	once: false,
-	execute(member: GuildMember) {
+	async execute(member: GuildMember) {
+		console.log(member)
 		// if the user has location in server map then remove it from server map.
 	},
 };
