7b4e9c8e7f496dda8946db8c9eea25599c06772a
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-16 19:56:18 -0700

    created api log model

---
+++ src/server/db/models/log/api-log.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model } from 'sequelize'
+import sequelize from "../../sequelize";
+
+class APILog extends Model {
+    declare method: string
+    declare path: string
+    declare status: number
+    declare responseTimeMS: number
+}
+
+APILog.init({
+    method: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    path: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    responseTimeMS: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    }
+}, {
+    sequelize: sequelize,
+    modelName: 'User',
+    timestamps: false
+});
+
+APILog.sync()
+
+export default APILog
