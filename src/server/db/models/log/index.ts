3fed309a490039467d9810dce0257054cd02fe13
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-16 20:48:20 -0700

    replaced processlog with applog

--- src/server/db/models/log/index.ts
+++ src/server/db/models/log/index.ts
@@ -1,9 +1,9 @@
-import ProcessLog from "./process-log";
+import AppLog from "./app-log";
 import BotLog from "./bot-log";
 import APILog from "./api-log";
 
 export {
-    ProcessLog,
+    AppLog,
     BotLog,
     APILog,
 }
