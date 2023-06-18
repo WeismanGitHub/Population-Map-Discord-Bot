8577392b89a562e4a062c67942190f6d3061f394
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-17 21:09:18 -0700

    finished

--- src/server/commands/map.ts
+++ src/server/commands/map.ts
@@ -1,6 +1,12 @@
+import { CustomClient } from '../custom-client'
 import {
 	SlashCommandBuilder,
 	CommandInteraction,
+	ActionRowBuilder,
+	StringSelectMenuBuilder,
+	StringSelectMenuOptionBuilder,
+	ButtonBuilder,
+	ButtonStyle,
 } from 'discord.js'
 
 export default {
@@ -10,5 +16,42 @@ export default {
 		.setDescription("Get a server map for a specific country.")
 	,
 	async execute(interaction: CommandInteraction): Promise<void> {
+		const client = interaction.client as CustomClient
+
+        const menuRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId(JSON.stringify({
+                    type: 'map-country',
+                    data: {}
+                }))
+                .setPlaceholder('Select a country!')
+                .addOptions(
+                    client.countries.slice(0, 25).map(country => 
+                        new StringSelectMenuOptionBuilder()
+                            .setLabel(country.name)
+                            .setValue(country.code)
+                    )
+                )
+        )
+
+        const buttonsRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+                .setLabel('⏪')
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId('0')
+                .setDisabled(true),
+            new ButtonBuilder()
+                .setLabel('⏩')
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId(JSON.stringify({
+                    type: 'map-country-page',
+                    data: { page: 1 }
+                }))
+        )
+
+        interaction.reply({
+            ephemeral: true,
+            components: [menuRow, buttonsRow]
+        })
 	}
 }
