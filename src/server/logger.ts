042d272cdc77a16946975bebfa6bc53c7548adcc
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-24 11:58:59 -0700

    got logger working

--- src/server/logger.ts
+++ src/server/logger.ts
@@ -5,12 +5,16 @@ import config from './config'
 
 const wbsTransport = new wbs({
     db: resolve(__dirname, '../../db.sqlite'),
-    table: 'log',
+    table: 'Log',
     params: ['level', 'resource', 'query', 'message']
 })
 
 export default winston.createLogger({
-    level: 'info',
+    levels: {
+        'error': 0,
+        'warn': 1,
+        'info': 2,
+    },
     format: winston.format.json(),
     transports: [
         config.mode === 'prod' ? wbsTransport : new winston.transports.Console()
