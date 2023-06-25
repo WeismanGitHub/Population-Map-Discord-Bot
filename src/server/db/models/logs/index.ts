f7b9cb8da8d9a1433ed133d7fbd8d5b05f563a4d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-25 14:03:33 -0700

    added log class

--- src/server/db/models/logs/index.ts
+++ src/server/db/models/logs/index.ts
@@ -1,9 +1,11 @@
 import AppLog from "./app-log";
 import APILog from "./api-log";
 import BotLog from "./bot-log";
+import Log from "./log";
 
 export {
     AppLog,
     APILog,
-    BotLog
+    BotLog,
+    Log
 }
