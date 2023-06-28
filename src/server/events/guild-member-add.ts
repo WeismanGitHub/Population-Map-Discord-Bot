7488fdcedf4424fb77aa233836190e173efd1107
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-27 18:59:04 -0700

    added logDescription

--- src/server/events/guild-member-add.ts
+++ src/server/events/guild-member-add.ts
@@ -3,15 +3,14 @@ import { User } from '../db/models'
 
 export default {
 	name: Events.GuildMemberAdd,
+	logDescription: "Add user's location to a guild map on join.",
 	once: false,
 	async execute(member: GuildMember) {
 		if (!member.user.bot) return
 		
 		const user = await User.findOne({ where: { discordID: member.id } })
 
-		if (!user) return
-
-		if (!user.addLocationOnJoin) return
+		if (!user || !user.addLocationOnJoin) return
 
 		// add the users location to the server map.
 	},
