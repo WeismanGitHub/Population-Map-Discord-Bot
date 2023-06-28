31b2972cb7cb2e37de85f2ed4970f188fb0b1365
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-27 19:00:23 -0700

    added logDescription

--- src/server/events/guild-member-remove.ts
+++ src/server/events/guild-member-remove.ts
@@ -3,6 +3,7 @@ import { Events, GuildMember } from 'discord.js'
 export default {
 	name: Events.GuildMemberRemove,
 	once: false,
+	logDescription: "remove user's location from a guild map on leave.",
 	async execute(member: GuildMember) {
 		if (!member.user.bot) return
 
