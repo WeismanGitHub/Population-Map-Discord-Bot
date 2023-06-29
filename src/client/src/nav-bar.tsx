a31e43f2fe7fe04079e4f8cbef7a145768bdc698
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-28 17:45:47 -0700

    added home and logout buttons

--- src/client/src/nav-bar.tsx
+++ src/client/src/nav-bar.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import ky from 'ky'
+import { errorToast, successToast } from './toasts'
 
 export default function NavBar() {
+    function logout() {
+        if (!window.confirm('Are you sure you want to logout?')) {
+            return
+        }
+
+        ky.post('/api/v1/auth/logout')
+        .then(res => {
+            successToast('Logged out!')
+        })
+        .catch(err => errorToast('Could not log out.'))
+    }
 
     return (<>
+        <div>
+            <a href="/">home</a>
+            <div onClick={logout}>logout</div>
+        </div>
     </>)
 }
