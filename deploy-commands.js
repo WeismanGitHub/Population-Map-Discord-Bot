b7826862480a8bbce1bf9a1c27c0a0784a6742bf
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-30 23:25:44 -0700

    deploy global/support guild commands now

--- deploy-commands.js
+++ deploy-commands.js
@@ -27,22 +27,33 @@ function getPaths(dir) {
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 const commandsPaths = getPaths(join(__dirname, 'dist\\server\\commands')).filter(file => file.endsWith('.js'))
-const commands = [];
+const supportServerCommands = [];
+const globalCommands = [];
 
 for (const path of commandsPaths) {
     const command = require(path);
 
-    if (!command.default.data) {
-        console.log(`malformed command file...`)
-        continue
+    if (!Object.keys(command.default).every(key => ['globalCommand', 'data', 'execute'].includes(key))) {
+        throw new Error('malformed command file...')
     }
 
-    commands.push(command.default.data.toJSON());
+    if (command.default.globalCommand) {
+        globalCommands.push(command.default.data.toJSON());
+    } else {
+        supportServerCommands.push(command.default.data.toJSON());
+    }
 }
 
 rest.put(
     Routes.applicationCommands(process.env.BOT_ID),
-    { body: commands },
+    { body: globalCommands },
 ).then(res => {
-    console.log(`reloaded ${commands.length} commands...`);
+    console.log(`deployed ${globalCommands.length} global commands...`);
+})
+
+rest.put(
+    Routes.applicationGuildCommands(process.env.BOT_ID, process.env.SUPPORT_SERVER_ID),
+    { body: supportServerCommands },
+).then(res => {
+    console.log(`deployed ${supportServerCommands.length} support server commands...`);
 })
