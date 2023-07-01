1b9c8a07c2669ba2226877c86668da78db717ba4
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-01 14:05:46 -0700

    deleted privileged user model

--- src/server/db/models/index.ts
+++ src/server/db/models/index.ts
@@ -1,10 +1,8 @@
-import PrivilegedUser from './privileged-user'
 import GuildMap from './guild-map'
 import Guild from './guild'
 import User from './user'
 
 export {
-    PrivilegedUser,
     GuildMap,
     Guild,
     User,
