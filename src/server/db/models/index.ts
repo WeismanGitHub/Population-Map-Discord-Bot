8f59e9be12e189a2fad24841a326b883b999e383
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-06-30 21:08:33 -0700

    added PrivilegedUser model to index file for models

--- src/server/db/models/index.ts
+++ src/server/db/models/index.ts
@@ -1,8 +1,10 @@
+import PrivilegedUser from './privileged-user'
 import GuildMap from './guild-map'
 import Guild from './guild'
 import User from './user'
 
 export {
+    PrivilegedUser,
     GuildMap,
     Guild,
     User,
