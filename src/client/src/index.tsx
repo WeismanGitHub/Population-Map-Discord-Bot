517328b8f0cbcd528a28c3a4a9cfbb1d1cbf3a34
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-04 15:48:49 -0700

    installed copyfiles

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -19,15 +19,15 @@ createRoot(document.getElementById("root")!)
 .render(<>
     <RouterProvider router={router}/>
     <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
-        />
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+    />
 </>);
