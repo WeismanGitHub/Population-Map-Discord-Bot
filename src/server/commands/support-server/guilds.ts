b508f52cca95ebd2aec19796a5b74ccd2551027f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-04 17:28:24 -0700

    created /servers command

---
+++ src/server/commands/support-server/guilds.ts
@@ -0,0 +1,54 @@
+import { guildEmbed } from '../../utils/embeds'
+import { ForbiddenError } from '../../errors'
+import { User } from '../../db/models'
+import {
+	SlashCommandBuilder,
+	CommandInteraction,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+} from 'discord.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('servers')
+		.setDescription("[Privileged Users Only] See the servers this bot is in.")
+		.setDMPermission(false)
+	,
+	globalCommand: false,
+	async execute(interaction: CommandInteraction){
+		const user = await User.findOne({ where: { discordID: interaction.user.id } })
+
+		if (!user) {
+			throw new ForbiddenError('You are not in the database.')
+		}
+
+		if (user.role !== 'admin') {
+			throw new ForbiddenError('You are not an admin.')
+		}
+
+        const guildEmbeds = interaction.client.guilds.cache.map((guild) => guildEmbed(guild))
+
+        const buttonsRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+                .setLabel('⏪')
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId('0')
+                .setDisabled(true),
+            new ButtonBuilder()
+                .setLabel('⏩')
+                .setDisabled(guildEmbeds.length < 10)
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId(JSON.stringify({
+                    type: 'guilds',
+                    data: { page: 1 }
+                }))
+        )
+
+        interaction.reply({
+            ephemeral: true,
+            embeds: guildEmbeds,
+            components: [buttonsRow]
+        })
+	}
+}
