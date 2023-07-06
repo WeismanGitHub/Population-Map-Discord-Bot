e05d568dd8068cdcc25c5f890805fe5a1c1dbcbe
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-05 19:39:17 -0700

    use updated properties on geojson

--- src/client/src/map.tsx
+++ src/client/src/map.tsx
@@ -31,14 +31,14 @@ export default function Map(props: { geojson: {}[], label: string }) {
             ref={chartRef}
             type="choropleth"
             data={{
-                labels: geojson.map((d: any) => d.properties.NAME_1),
+                labels: geojson.map((d: any) => d.properties.name),
                 datasets: [
                     {
                         outline: geojson,
                         label: props.label,
                         data: geojson.map((d: any) => ({
                             feature: d,
-                            value: d.count
+                            value: d.amount
                         })),
                     }
                 ]
