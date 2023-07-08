cf6a1580165369413f1c98281e959d7a939c7f92
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-08 16:36:04 -0700

    added GuildCountry to models index

--- src/server/db/models/index.ts
+++ src/server/db/models/index.ts
@@ -1,8 +1,11 @@
 import GuildCountries from './guild-countries'
+import GuildCountry from './guild-country'
 import Guild from './guild'
 import User from './user'
+
 export {
     GuildCountries,
+    GuildCountry,
     Guild,
     User,
 }
