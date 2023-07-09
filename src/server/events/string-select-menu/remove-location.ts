1b234e93fde32ca992022e300a32d121c5643a82
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-09 04:17:44 -0700

    created remove-location string select menu event

---
+++ src/server/events/string-select-menu/remove-location.ts
@@ -0,0 +1,48 @@
+import { guildEmbed } from '../../utils/embeds';
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Events,
+    StringSelectMenuInteraction,
+    Interaction
+} from "discord.js"
+
+export default {
+	name: Events.InteractionCreate,
+	once: false,
+    check: async (interaction: Interaction) => {
+        if (!interaction.isStringSelectMenu()) return;
+
+        const customID: CustomID<{}> = JSON.parse(interaction.customId)
+
+        if (customID.type !== 'remove-location') return
+
+        return { customID, interaction }
+    },
+    execute: async ({ interaction, customID }: {
+        interaction: StringSelectMenuInteraction,
+        customID: CustomID<{}>
+    }) => {
+        const guildID = interaction.values[0]
+        console.log(interaction)
+
+        const guild = await interaction.client.guilds.fetch(guildID)
+
+        const confirmationButtonRow = new ActionRowBuilder<ButtonBuilder>()
+		.addComponents(
+            new ButtonBuilder()
+                .setLabel(`Remove Location?`)
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId(JSON.stringify({
+                    type: 'remove-location-confirmation',
+                    data: {}
+                }))
+		)
+
+        interaction.update({
+            embeds: [guildEmbed(guild)],
+            components: [confirmationButtonRow]
+        })
+    }
+}
