4f2716057478375b51505ed73e39810a01f636d2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-08 20:20:09 -0700

    finished /remove-location

--- src/server/commands/global/remove-location.ts
+++ src/server/commands/global/remove-location.ts
@@ -1,4 +1,7 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js'
+import { Guild, User } from '../../db/models'
+import { NotFoundError } from '../../errors'
+import { infoEmbed } from '../../utils/embeds'
 
 export default {
 	data: new SlashCommandBuilder()
@@ -8,5 +11,24 @@ export default {
 	,
 	guildIDs: null,
 	async execute(interaction: ChatInputCommandInteraction) {
+		const guildID = interaction.guildId!
+		const guild = await Guild.findOne({ where: { ID: guildID! } })
+
+        if (!guild) {
+            throw new NotFoundError('This server is not in the database.')
+        }
+		
+		const user = await User.findOne({ where: { discordID: interaction.user.id } })
+
+		if (!user) {
+			throw new NotFoundError('Could not find you in the database.')
+		}
+
+		await user.removeLocationFromGuild(guildID)
+
+		interaction.reply({
+			ephemeral: true,
+			embeds: [infoEmbed('Your location has been removed!')]
+		})
 	}
 }
