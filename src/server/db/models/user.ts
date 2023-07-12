596597e95b3e396c11cc07d45c7d80dab697ba6b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-11 20:43:09 -0700

    got user.destroy method working

--- src/server/db/models/user.ts
+++ src/server/db/models/user.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { BadRequestError, InternalServerError } from "../../errors";
 import GuildCountries from "./guild-countries";
 import GuildCountry from "./guild-country";
@@ -6,7 +7,8 @@ import {
     DataTypes,
     InferAttributes,
     InferCreationAttributes,
-    Model
+    Model,
+    Transaction
 } from 'sequelize'
 
 class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
@@ -16,8 +18,11 @@ class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
     declare addLocationOnJoin?: boolean
     declare role?: 'regular' | 'admin'
     declare guildIDs?: string[]
-    declare addLocationToGuild: (guildID: string) => Promise<void>
-    declare removeLocationFromGuild: (guildID: string) => Promise<void>
+
+    declare addLocationToGuild: (guildID: string, transaction?: Transaction) => Promise<void>
+    declare removeLocationFromGuild: (guildID: string, transaction?: Transaction, commit: boolean=true) => Promise<void>
+    declare updateLocation: (countryCode: string | null, subdivisionCode: string | null) => Promise<void>
+    declare delete: () => Promise<void>
 }
 
 User.init({
@@ -66,7 +71,7 @@ User.init({
     timestamps: false
 });
 
-User.prototype.addLocationToGuild = async function(guildID) {
+User.prototype.addLocationToGuild = async function(guildID, transaction?: Transaction) {
     const guildIDs = this.guildIDs
 
     if (!guildIDs) {
@@ -81,25 +86,69 @@ User.prototype.addLocationToGuild = async function(guildID) {
     const guildCountry = this.subdivisionCode ? new GuildCountry(guildID, this.countryCode) : null
 
     if (guildCountry) await guildCountry.sync()
+    transaction = transaction || await sequelize.transaction()
 
-    await sequelize.transaction(async (transaction) => {
+    try {
         await guildCountries.increaseCountryCount(this.countryCode, transaction)
         await this.update({ guildIDs: [...guildIDs, guildID] }, { transaction })
-
+    
         if (guildCountry && this.subdivisionCode) {
             await guildCountry.increaseSubdivisionCount(this.subdivisionCode, transaction)
         }
-    }).catch(err => {
+
+        await transaction.commit()
+    } catch(err) {
+        await transaction.rollback()
         throw new InternalServerError('Could not save location to database.')
-    })
+    }
 }
 
-User.prototype.removeLocationFromGuild = async function(guildID: string) {
+User.prototype.removeLocationFromGuild = async function(guildID: string, transaction?: Transaction, commit: boolean=true) {
     const guildIDs = this.guildIDs
 
     if (!guildIDs || !guildIDs.includes(guildID)) {
         throw new BadRequestError('Your location has not been saved in this server.')
     }
+
+    const guildCountries = new GuildCountries(guildID)
+    const guildCountry = this.subdivisionCode ? new GuildCountry(guildID, this.countryCode) : null
+
+    transaction = transaction || await sequelize.transaction()
+
+    try {
+        await guildCountries.decreaseCountryCount(this.countryCode, transaction)
+        await this.update({ guildIDs: guildIDs.filter(id=> id !== guildID) }, { transaction })
+
+        if (guildCountry && this.subdivisionCode) {
+            await guildCountry.decreaseSubdivisionCount(this.subdivisionCode, transaction)
+        }
+        
+        if (commit) await transaction.commit()
+    } catch(err) {
+        if (commit) await transaction.rollback()
+
+        throw new InternalServerError('Could not remove location from database.')
+    }
+}
+
+User.prototype.updateLocation = async function(countryCode: string | null, subdivisionCode: string | null) {
+    const guildIDs = this.guildIDs
+
+    if (!guildIDs) {
+        return
+    }
+
+    await sequelize.transaction(async (transaction) => {
+        await this.update({ countryCode }, { transaction })
+        
+        await Promise.all(guildIDs.map(guildID => {
+            return this.removeLocationFromGuild(guildID, transaction)
+        }))
+        
+        await Promise.all(guildIDs.map(guildID => {
+            return this.addLocationToGuild(guildID, transaction)
+        }))
+    }).catch(err => { throw new InternalServerError('Could not update your location.') })
 
     const guildCountries = new GuildCountries(guildID)
     const guildCountry = this.subdivisionCode ? new GuildCountry(guildID, this.countryCode) : null
@@ -113,6 +162,22 @@ User.prototype.removeLocationFromGuild = async function(guildID: string) {
         }
     }).catch(err => {
         throw new InternalServerError('Could not remove location from database.')
+    })
+}
+
+User.prototype.delete = async function() {
+    const guildIDs = this.guildIDs
+
+    await sequelize.transaction(async (transaction) => {
+        if (guildIDs) {
+            await Promise.all(guildIDs.map(guildID => {
+                return this.removeLocationFromGuild(guildID, transaction, false)
+            }))
+        }
+
+        await this.destroy({ transaction })
+    }).catch(err => {
+        throw new InternalServerError('Could not delete your data.')
     })
 }
 
