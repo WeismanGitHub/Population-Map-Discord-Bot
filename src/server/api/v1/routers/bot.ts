decfac1029241c5f279c9a118fce580b727596c0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-12 13:26:00 -0700

    created bot router

---
+++ src/server/api/v1/routers/bot.ts
@@ -0,0 +1,8 @@
+import { getBotData } from '../controllers/bot'
+import { Router } from 'express';
+
+const botRouter: Router = Router();
+
+botRouter.get('/', getBotData)
+
+export default botRouter
