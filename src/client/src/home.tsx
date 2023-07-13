b02149185c329f36c723423f218e902000d07e6d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-07-13 04:30:30 -0700

    show bot info on homepage

--- src/client/src/home.tsx
+++ src/client/src/home.tsx
@@ -1,9 +1,38 @@
+import React, { useEffect, useState } from 'react'
 import NavBar from './nav-bar'
-import React from 'react'
+import ky from 'ky'
+
+interface BotAPIRes {
+    guildCount: number
+    botInviteURL: string
+    supportServerInvite: string
+    githubURL: string
+    buyMeACoffeeURL: string
+}
 
 export default function Home() {
+    const [res, setRes] = useState<BotAPIRes>()
 
-    return (<>
+    useEffect(() => {
+        ky.get('/api/v1/bot/').json().then(res => {
+            setRes(res as BotAPIRes)
+        })
+    }, [])
+
+    return (<div style={{ textAlign: 'center', fontSize: 'x-large' }}>
         <NavBar/>
-    </>)
+        In {res?.guildCount} Servers
+        <br/>
+        <br/>
+        <a href={res?.botInviteURL}>Bot Invite</a>
+        <br/>
+        <br/>
+        <a href={res?.supportServerInvite}>Support Server</a>
+        <br/>
+        <br/>
+        <a href={res?.githubURL}>Github</a>
+        <br/>
+        <br/>
+        <a href={res?.buyMeACoffeeURL}>Buy Me a Coffee</a>
+    </div>)
 }
