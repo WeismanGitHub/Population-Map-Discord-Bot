471a8a96f40cf45a2ce74efc6970550785d41d0e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-09 09:23:17 -0800

    improved login/logout button display

--- src/client/src/nav-bar.tsx
+++ src/client/src/nav-bar.tsx
@@ -1,8 +1,10 @@
 import { errorToast, successToast } from './toasts'
-import React from 'react'
+import React, { useState } from 'react'
 import ky from 'ky'
 
 export default function NavBar() {
+    const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("loggedIn")))
+
     function logout() {
         if (!window.confirm('Are you sure you want to logout?')) {
             return
@@ -11,6 +13,7 @@ export default function NavBar() {
         ky.post('/api/v1/auth/logout')
         .then(res => {
             localStorage.removeItem("loggedIn")
+            setLoggedIn(false)
             successToast('Logged out!')
         })
         .catch(err => errorToast('Could not log out.'))
@@ -20,7 +23,7 @@ export default function NavBar() {
         <div className='navbar'>
             <a className='navbar-button' href="/">home</a>
             {
-                localStorage.getItem("loggedIn") ?
+                loggedIn ?
                 <div className='navbar-button' onClick={logout}>logout</div> :
                 <a className='navbar-button' href="/discord/oauth2">login</a>
             }
