003387142ebf0b6aa738883231c57871e8aa0d4e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-09 17:13:15 -0800

    created event

---
+++ src/server/events/guild-member-remove.ts
@@ -0,0 +1,17 @@
+import { Events, GuildMember } from 'discord.js'
+import { GuildLocation } from '../db/models';
+
+export default {
+	name: Events.GuildMemberRemove,
+	once: false,
+	check: async (member: GuildMember) => {
+		return member
+    },
+    execute: async ({ member }: {
+        member: GuildMember,
+    }) => {
+		if (!member.user.bot) return
+		
+		await GuildLocation.destroy({ where: { guildID: member.guild.id, userID: member.user.id } })
+	},
+};
