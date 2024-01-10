054ac2affb8b62ada61226a0592906ff0e87c2e7
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-09 20:48:13 -0800

    works

--- src/server/events/guild-member-remove.ts
+++ src/server/events/guild-member-remove.ts
@@ -5,12 +5,12 @@ export default {
 	name: Events.GuildMemberRemove,
 	once: false,
 	check: async (member: GuildMember) => {
-		return member
+		return { member }
     },
     execute: async ({ member }: {
         member: GuildMember,
     }) => {
-		if (!member.user.bot) return
+		if (member.user.bot) return
 		
 		await GuildLocation.destroy({ where: { guildID: member.guild.id, userID: member.user.id } })
 	},
