3e3707e4ed6700fe1254b56970b2e27c6de8a76f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-13 22:29:15 -0800

    add user-role to member when location is set

--- src/server/events/string-select-menu/location-subdivision.ts
+++ src/server/events/string-select-menu/location-subdivision.ts
@@ -1,6 +1,6 @@
-import { Events, Interaction, StringSelectMenuInteraction } from 'discord.js';
-import { InternalServerError } from '../../errors';
-import { GuildLocation } from '../../db/models';
+import { DiscordAPIError, Events, Interaction, StringSelectMenuInteraction } from 'discord.js';
+import { Guild, GuildLocation } from '../../db/models';
+import { ForbiddenError, InternalServerError, NotFoundError } from '../../errors';
 import { infoEmbed } from '../../utils/embeds';
 
 export default {
@@ -22,6 +22,16 @@ export default {
         interaction: StringSelectMenuInteraction;
         customID: CustomID<{ countryCode: string }>;
     }) => {
+        if (!interaction.inGuild()) return;
+
+        const guild = await Guild.findOne({ where: { guildID: interaction.guildId } }).catch((err) => {
+            throw new InternalServerError('Could not get this server.');
+        });
+
+        if (!guild) {
+            throw new InternalServerError('This server has not been set up.');
+        }
+
         const subdivisionCode = interaction.values[0];
         const countryCode = customID.data.countryCode;
 
@@ -33,6 +43,28 @@ export default {
         }).catch((err) => {
             throw new InternalServerError('Could not save your subdivision.');
         });
+
+        if (guild.userRoleID) {
+            const member = await interaction.guild?.members.fetch(interaction.user.id);
+
+            if (!member) {
+                throw new NotFoundError("Could not find member.")
+            }
+
+            await member.roles.add(guild.userRoleID).catch(async (err: DiscordAPIError) => {
+                await GuildLocation.destroy({
+                    where: { userID: interaction.user.id, guildID: interaction.guildId },
+                });
+
+                if (err.status === 404) {
+                    throw new NotFoundError('Could not find user-role.');
+                } else if (err.status == 403) {
+                    throw new ForbiddenError('Missing permissions to add user-role.');
+                }
+
+                throw new InternalServerError('Could not add user-role.');
+            });
+        }
 
         interaction.update({
             embeds: [infoEmbed('Selected a country and subdivision!')],
