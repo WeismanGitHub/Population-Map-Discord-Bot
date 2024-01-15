0547182f4e65b3c11806ddc1260b8b1f7be285df
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-15 08:43:45 -0800

    remove x

--- src/server/utils/letters.ts
+++ src/server/utils/letters.ts
@@ -1,4 +1,4 @@
-const alphabet = ['A', 'Å', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const alphabet = ['A', 'Å', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z']
 
 const letterPageMap: Record<string, number> = {
     'A': 0,
