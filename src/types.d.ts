b63443f495e15b156a030136e180a1495f8a26d6
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-16 21:14:11 -0800

    created Subdivision and CountryLetters

--- src/types.d.ts
+++ src/types.d.ts
@@ -11,3 +11,51 @@ declare global {
         }
     }
 }
+
+type Subdivision = {
+    code: string;
+    name: string;
+    sub: (
+        | {
+              countryName: string;
+              countryCode: string;
+              code: string;
+              regionCode: string;
+              type: string;
+              name: string;
+          }
+        | {
+              type: string;
+              name: string;
+              code: string;
+          }
+    )[];
+};
+
+type CountryLetters =
+    | 'A'
+    | 'Å'
+    | 'B'
+    | 'C'
+    | 'D'
+    | 'E'
+    | 'F'
+    | 'G'
+    | 'H'
+    | 'I'
+    | 'J'
+    | 'K'
+    | 'L'
+    | 'M'
+    | 'N'
+    | 'O'
+    | 'P'
+    | 'Q'
+    | 'R'
+    | 'S'
+    | 'T'
+    | 'U'
+    | 'V'
+    | 'W'
+    | 'Y'
+    | 'Z';
