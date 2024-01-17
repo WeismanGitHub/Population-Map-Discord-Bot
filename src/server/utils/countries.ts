6b7abdc11833153c99e8d1710f96d4b7ee399017
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-16 23:22:50 -0800

    used prettier

--- src/server/utils/countries.ts
+++ src/server/utils/countries.ts
@@ -2,7 +2,7 @@ import { InternalServerError } from '../errors';
 import iso from 'iso-3166-2';
 
 class ISO31662 {
-    public readonly countries: Record<CountryLetter, Country[]>
+    public readonly countries: Record<CountryLetter, Country[]>;
 
     constructor() {
         const map: Record<string, Country[]> = {};
@@ -13,63 +13,63 @@ class ISO31662 {
                     return { code: sub[0], ...sub[1] };
                 })
                 .sort((a, b) => a.name.localeCompare(b.name));
-        
+
             const letter = data[1].name.slice(0, 1);
             const country = { name: data[1].name, sub: subdivisions, code: data[0] };
-        
+
             if (!map[letter]) {
                 map[letter] = [country];
             } else {
                 map[letter].push(country);
             }
         });
-        
+
         const countries: Record<string, Country[]> = {};
-        
+
         Object.entries(map).forEach((data) => {
             countries[data[0]] = data[1].sort((a, b) => a.name.localeCompare(b.name));
         });
 
-        this.countries = countries as Record<CountryLetter, Country[]>
+        this.countries = countries as Record<CountryLetter, Country[]>;
     }
 
     // implement binary search instead of using countries. hash map?
     // Might not be worth it tho since its only like 250 countries in total.
     public getCountry(code: string) {
-        const countries: Country[] | undefined = this.countries[code.slice(0, 1) as CountryLetter]
+        const countries: Country[] | undefined = this.countries[code.slice(0, 1) as CountryLetter];
         let country: Country | undefined;
 
         if (countries) {
-            countries.forEach(c => {
+            countries.forEach((c) => {
                 if (c.code === code) {
-                    country = c
+                    country = c;
                 }
-            })
+            });
         }
 
         if (country) {
-            return country
+            return country;
         }
 
         Object.entries(this.countries).forEach(([letter, countries]) => {
             if (letter == code.slice(0, 1)) {
-                return
+                return;
             }
 
-            countries.forEach(c => {
+            countries.forEach((c) => {
                 if (c.code === code) {
-                    country = c
+                    country = c;
                 }
-            })
-        })
+            });
+        });
 
         if (!country) {
-            throw new InternalServerError("Could not find country.")
+            throw new InternalServerError('Could not find country.');
         }
 
-        return country
+        return country;
     }
 }
 
-const iso31662 = new ISO31662()
-export default iso31662
+const iso31662 = new ISO31662();
+export default iso31662;
