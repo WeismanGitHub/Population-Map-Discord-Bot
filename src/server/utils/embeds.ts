eb32115b3e60bc5110595ed5808d11240b69247f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-02-02 17:25:33 -0800

    changed embeds to be OO

--- src/server/utils/embeds.ts
+++ src/server/utils/embeds.ts
@@ -1,38 +1,43 @@
 import { EmbedBuilder, Guild } from 'discord.js';
 
-function errorEmbed(description: string | null = null, statusCode: number | null = null): EmbedBuilder {
-    return new EmbedBuilder()
-        .setTitle("There's been an error!")
-        .setDescription(description)
-        .setColor('#FF0000') // Red
-        .setFooter({ text: `Status Code: ${statusCode ?? 'Unknown'}` });
+class ErrorEmbed extends EmbedBuilder {
+    constructor(description: string | null = null, statusCode: number | null = null) {
+        super();
+
+        this.setTitle("There's been an error!")
+            .setDescription(description)
+            .setColor('#FF0000') // Red
+            .setFooter({ text: `Status Code: ${statusCode ?? 'Unknown'}` });
+    }
 }
 
-function infoEmbed(
-    title: string | null,
-    description: null | string = null,
-    footer: null | string = null
-): EmbedBuilder {
-    return new EmbedBuilder()
-        .setTitle(title)
-        .setColor('#8F00FF') // Purple
-        .setDescription(description)
-        .setFooter(footer ? { text: footer } : null);
+class InfoEmbed extends EmbedBuilder {
+    constructor(title: string | null, description: null | string = null, footer: null | string = null) {
+        super();
+
+        this.setTitle(title)
+            .setColor('#8F00FF') // Purple
+            .setDescription(description)
+            .setFooter(footer ? { text: footer } : null);
+    }
 }
 
-function guildEmbed(guild: Guild) {
-    const joinedDate = new Date(guild.joinedTimestamp).toLocaleDateString('en-US', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    });
+class GuildEmbed extends EmbedBuilder {
+    constructor(guild: Guild) {
+        super();
 
-    return new EmbedBuilder()
-        .setTitle(guild.name)
-        .setColor('#8F00FF') // Purple
-        .setImage(guild.iconURL())
-        .setFooter({ text: `Joined: ${joinedDate}` });
+        const joinedDate = new Date(guild.joinedTimestamp).toLocaleDateString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+
+        this.setTitle(guild.name)
+            .setColor('#8F00FF') // Purple
+            .setImage(guild.iconURL())
+            .setFooter({ text: `Joined: ${joinedDate}` });
+    }
 }
 
-export { errorEmbed, infoEmbed, guildEmbed };
+export { ErrorEmbed, InfoEmbed, GuildEmbed };
