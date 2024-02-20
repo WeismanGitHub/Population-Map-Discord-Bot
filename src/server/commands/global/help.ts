42fa131f54191e2c9ca48e86d06490b44635e972
weisman <weisman123@outlook.com>
2024-02-19 16:11:30 -0800

    use img from github

--- src/server/commands/global/help.ts
+++ src/server/commands/global/help.ts
@@ -1,32 +1,23 @@
 import { InfoEmbed } from '../../utils/embeds';
 import config from '../../config';
-import { resolve } from 'path';
 import {
     SlashCommandBuilder,
     CommandInteraction,
     ActionRowBuilder,
     ButtonBuilder,
     ButtonStyle,
-    AttachmentBuilder,
 } from 'discord.js';
 
 export default {
     data: new SlashCommandBuilder().setName('help').setDescription('Information about this bot.'),
     guildIDs: null,
     async execute(interaction: CommandInteraction): Promise<void> {
-        const attachment = new AttachmentBuilder(
-            resolve(__dirname, '../../../../../../images/WORLD2-example.jpg'),
-            {
-                name: 'world-example.jpg',
-            }
-        );
-
         const embed = new InfoEmbed(
             null,
             'The Population Map Bot is a dynamic map generator capable of visualizing population data on a global, continental, or country level. Maps are generated from self-reported locations provided by Discord server members, enabling users to explore population distributions with ease.\n\nThis bot generates a unique map for each Discord server that can be accessed with `/map`. Server members use `/set-location` to add their location to the map. A location can be any country and, optionally, a subdivision (state, region, prefecture, etc.) within that country. Server admins can also make it so only members who share their location can access the server.'
         )
             .addFields({ name: 'Contact the Creator:', value: `<@${config.mainAccountID}>` })
-            .setImage('attachment://world-example.jpg');
+            .setImage('https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/WORLD2-example.jpg');
 
         const linksRow = new ActionRowBuilder<ButtonBuilder>().addComponents([
             new ButtonBuilder().setLabel('Website').setURL(config.websiteURL).setStyle(ButtonStyle.Link),
@@ -70,7 +61,6 @@ export default {
             embeds: [embed],
             components: interaction.guild ? [firstRow, linksRow, mapButtonsRow] : [firstRow, linksRow],
             ephemeral: true,
-            files: [attachment],
         });
     },
 };
