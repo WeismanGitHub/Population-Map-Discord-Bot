21fc7fedac0006d0926d037742717c49ee86b35a
weisman <weisman123@outlook.com>
2024-02-21 00:37:19 -0800

    check if sendmessage perm

--- src/server/events/guild-create.ts
+++ src/server/events/guild-create.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, Guild } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, Guild, PermissionFlagsBits } from 'discord.js';
 import { InfoEmbed } from '../utils/embeds';
 import config from '../config';
 
@@ -7,16 +7,19 @@ export default {
     once: false,
     check: async (guild: Guild) => guild,
     execute: async (guild: Guild) => {
-        const channel = guild.systemChannel
+        const channel = guild.systemChannel;
 
         if (!channel) return;
+        if (!guild?.members.me?.permissions.has(PermissionFlagsBits.SendMessages)) return;
 
         const embed = new InfoEmbed(
             null,
             'The Population Map Bot is a dynamic map generator capable of visualizing population data on a global, continental, or country level. Maps are generated from self-reported locations provided by Discord server members, enabling users to explore population distributions with ease.\n\nThis bot generates a unique map for each Discord server that can be accessed with `/map`. Server members use `/set-location` to add their location to the map. A location can be any country and, optionally, a subdivision (state, region, prefecture, etc.) within that country. Server admins can also make it so only members who share their location can access the server.'
         )
             .addFields({ name: 'Contact the Creator:', value: `<@${config.mainAccountID}>` })
-            .setImage('https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/WORLD2-example.jpg');
+            .setImage(
+                'https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/WORLD2-example.jpg'
+            );
 
         const linksRow = new ActionRowBuilder<ButtonBuilder>().addComponents([
             new ButtonBuilder().setLabel('Website').setURL(config.websiteURL).setStyle(ButtonStyle.Link),
@@ -47,7 +50,7 @@ export default {
 
         await channel.send({
             embeds: [embed],
-            components: [firstRow, linksRow]
-        })
+            components: [firstRow, linksRow],
+        });
     },
 };
