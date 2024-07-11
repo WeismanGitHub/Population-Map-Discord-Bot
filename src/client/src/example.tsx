b37fb6afa2c5d77e386f11692693322de7c39678
weisman <weisman123@outlook.com>
2024-07-10 17:04:15 -0700

    Create example map

---
+++ src/client/src/example.tsx
@@ -0,0 +1,74 @@
+import { Chart as ChartJS, CategoryScale, Tooltip, Title, Legend } from 'chart.js';
+import { Chart as ReactChart } from 'react-chartjs-2';
+import { useEffect, useRef, useState } from 'react';
+import * as ChartGeo from 'chartjs-chart-geo';
+import axios from 'axios';
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    ChartGeo.ChoroplethController,
+    ChartGeo.ProjectionScale,
+    ChartGeo.ColorScale,
+    ChartGeo.GeoFeature
+);
+
+export default function Example() {
+    const chartRef = useRef();
+    const [geojson, setGeojson] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const geojsonResponse = await axios.get(
+                `https://raw.githubusercontent.com/WeismanGitHub/Population-Density-Map-Discord-Bot/main/topojson/WORLD.json`
+            )
+
+            const features = Object.values(geojsonResponse.data.objects).map((feature) => {
+                // @ts-ignore
+                return ChartGeo.topojson.feature(geojsonResponse.data, feature);
+            });
+
+            geojsonResponse.data.features = features
+            setGeojson(geojsonResponse.data)
+        })()
+    }, [])
+
+    
+    return (<>
+        {geojson &&         <ReactChart
+            style={{ backgroundColor: 'white', borderRadius: '5px', maxHeight: '450px' }}
+            ref={chartRef}
+            type="choropleth"
+            data={{
+                // @ts-ignore
+                labels: geojson.features.map((d: any) => d.properties.name),
+                datasets: [
+                    {
+                // @ts-ignore
+                        outline: geojson.features,
+                // @ts-ignore
+                data: geojson.features.map((d: any) => ({
+                            feature: d,
+                            value: Math.round(Math.random() * 100)
+                        })),
+                    },
+                ],
+            }}
+            options={{
+                borderColor: 'black',
+                showOutline: true,
+                showGraticule: true,
+                plugins: {
+                    legend: { display: false },
+                },
+                scales: {
+                    xy: { projection: 'equalEarth' },
+                },
+            }}
+        />
+}
+    </>
+    );
+}
--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -4,14 +4,16 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+import Example from './example';
 import Login from './login';
 import Guild from './guild';
 import Home from './home';
 
 const router = createBrowserRouter([
-    { path: '/', element: <Home /> },
-    { path: '/maps/:guildID', element: <Guild /> },
     { path: '/discord/oauth2', element: <Login /> },
+    { path: '/maps/:guildID', element: <Guild /> },
+    { path: '/example', element: <Example /> },
+    { path: '/', element: <Home /> },
 ]);
 
 createRoot(document.getElementById('root')!).render(
