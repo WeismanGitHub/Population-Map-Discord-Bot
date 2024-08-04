adc6eb0e4997b787f9baf7298aeb1956fca4a3f1
weisman <weisman123@outlook.com>
2024-08-04 05:11:35 -0700

    Update homepage css

--- src/client/src/home.tsx
+++ src/client/src/home.tsx
@@ -1,5 +1,5 @@
+import { Carousel, Row } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
-import { Carousel } from 'react-bootstrap';
 import NavBar from './nav-bar';
 import axios from 'axios';
 
@@ -15,81 +15,81 @@ export default function Home() {
     }, []);
 
     return (
-        <div style={{ textAlign: 'center', fontSize: 'x-large' }}>
+        <>
             <NavBar />
-            <div className="container mb-2">
-                <div className="row">
-                    <div className="col-md-12 d-flex col-lg-6 flex-column justify-content-center align-items-center order-md-1 order-lg-2">
-                        <code className="m-1">Used by {info?.guildCount} Servers</code>
-                        <Carousel interval={3000} className="carousel-dark">
+            <div className="p-0 m-0 mt-1 overflow-x-hidden full-height-minus-navbar container-fluid w-100 flex-wrap m-0 d-flex justify-content-center align-content-center">
+                <Row className="fs-4 p-0 d-flex justify-content-center">
+                    <Row className="d-flex justify-content-center">
+                        <code className="m-1" style={{ width: 'fit-content', height: 'fit-content' }}>
+                            Used by {info?.guildCount} Servers
+                        </code>
+                    </Row>
+                    <Row className="d-flex justify-content-center">
+                        <Carousel
+                            keyboard={true}
+                            wrap={true}
+                            interval={3000}
+                            className="carousel-dark"
+                            style={{
+                                width: '50%',
+                                minWidth: '310px',
+                                borderRadius: '5px',
+                                overflow: 'hidden',
+                            }}
+                        >
                             <Carousel.Item>
                                 <img
                                     src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/WORLD2-example.jpg"
                                     alt="World Example"
-                                    className="image-custom"
-                                    style={{
-                                        width: '100%',
-                                        height: 'auto',
-                                    }}
+                                    className="example-image"
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
                                     src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/CONTINENTS-example.jpg"
                                     alt="Continents Example"
-                                    className="image-custom"
-                                    style={{
-                                        width: '100%',
-                                        height: 'auto',
-                                    }}
+                                    className="example-image"
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
                                     src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/US-example.jpg"
                                     alt="USA Example"
-                                    className="image-custom"
-                                    style={{
-                                        width: '100%',
-                                        height: 'auto',
-                                    }}
+                                    className="example-image"
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
                                     src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/IT-example.jpg"
                                     alt="Italy Example"
-                                    className="image-custom"
-                                    style={{
-                                        width: '100%',
-                                        height: 'auto',
-                                    }}
+                                    className="example-image"
                                 />
                             </Carousel.Item>
                         </Carousel>
-                    </div>
-                    <div className="col-md-12 col-lg-6 order-md-2 order-lg-1">
-                        <br />
+                    </Row>
+                </Row>
+                <Row className="fs-4 text-center" style={{ minWidth: '310px', width: '75%' }}>
+                    <p>
                         The Population Map Bot is a dynamic map generator that visualizes your Discord
                         server's population data on a global, continental, or country level. Maps are
                         generated from self-reported locations provided by server members and are anonymous.
-                        <br />
-                        <br />
+                    </p>
+                    <p>
                         Server members use `/set-location` to set their country and, optionally, a subdivision
                         (state, province, etc.) within that country. Maps are generated through the website,
                         and you can get a link to a server's map with `/map`. Server admins can also make it
-                        so only members who share their location can access the server.
+                        so only members who share their location can access the server.{' '}
                         <a
                             className="link"
                             href={
                                 'https://github.com/WeismanGitHub/Population-Map-Discord-Bot?tab=readme-ov-file#user-docs'
                             }
                         >
-                            Read more here...
+                            Read more here.
                         </a>
-                    </div>
-                </div>
+                    </p>
+                </Row>
             </div>
-        </div>
+        </>
     );
 }
