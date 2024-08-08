434dfb93a89aaa52617ee1654dbceefdc9bfb7c8
weisman <weisman123@outlook.com>
2024-08-08 16:11:10 -0700

    Fix carousel css

--- src/client/src/home.tsx
+++ src/client/src/home.tsx
@@ -19,56 +19,55 @@ export default function Home() {
             <NavBar />
             <div className="p-0 m-0 container-fluid w-100 flex-wrap d-flex justify-content-center align-content-center fs-4 overflow-x-hidden mt-2">
                 <Row className="d-flex justify-content-center">
-                    <Row className="d-flex justify-content-center">
-                        <Carousel
-                            keyboard={true}
-                            wrap={true}
-                            interval={3000}
-                            className="carousel-dark"
-                            style={{
-                                width: '50%',
-                                minWidth: '315px',
-                                borderRadius: '5px',
-                                overflow: 'hidden',
-                            }}
-                        >
-                            <Carousel.Item>
-                                <img
-                                    src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/WORLD2-example.jpg"
-                                    alt="World Example"
-                                    className="example-image"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/CONTINENTS-example.jpg"
-                                    alt="Continents Example"
-                                    className="example-image"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/US-example.jpg"
-                                    alt="USA Example"
-                                    className="example-image"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/IT-example.jpg"
-                                    alt="Italy Example"
-                                    className="example-image"
-                                />
-                            </Carousel.Item>
-                        </Carousel>
-                    </Row>
+                    <Carousel
+                        keyboard={true}
+                        wrap={true}
+                        interval={3000}
+                        className="carousel-dark"
+                        style={{
+                            width: '95%',
+                            maxWidth: '600px',
+                            minWidth: '310px',
+                            borderRadius: '5px',
+                            overflow: 'hidden',
+                        }}
+                    >
+                        <Carousel.Item>
+                            <img
+                                src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/WORLD2-example.jpg"
+                                alt="World Example"
+                                className="example-image"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/CONTINENTS-example.jpg"
+                                alt="Continents Example"
+                                className="example-image"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/US-example.jpg"
+                                alt="USA Example"
+                                className="example-image"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src="https://raw.githubusercontent.com/WeismanGitHub/Population-Map-Discord-Bot/main/images/IT-example.jpg"
+                                alt="Italy Example"
+                                className="example-image"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
                 </Row>
                 <Row className="d-flex justify-content-center mt-1 mb-1 w-100">
                     <code className="m-1" style={{ width: 'fit-content', height: 'fit-content' }}>
                         Used by {info?.guildCount} Servers
                     </code>
                 </Row>
-                <Row className="text-center" style={{ minWidth: '315px', width: '75%' }}>
+                <Row className="text-center" style={{ minWidth: '310px', width: '75%' }}>
                     <p>
                         The Population Map Bot is a dynamic map generator that visualizes your Discord
                         server's population data on a global, continental, or country level. Maps are
