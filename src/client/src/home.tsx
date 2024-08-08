66ce5a3c968ba30c56f6c130011fb8230d6b80fc
weisman <weisman123@outlook.com>
2024-08-08 14:58:07 -0700

    Change layout of homepage

--- src/client/src/home.tsx
+++ src/client/src/home.tsx
@@ -17,13 +17,8 @@ export default function Home() {
     return (
         <>
             <NavBar />
-            <div className="p-0 m-0 mt-1 overflow-x-hidden full-height-minus-navbar container-fluid w-100 flex-wrap m-0 d-flex justify-content-center align-content-center">
-                <Row className="fs-4 p-0 d-flex justify-content-center">
-                    <Row className="d-flex justify-content-center">
-                        <code className="m-1" style={{ width: 'fit-content', height: 'fit-content' }}>
-                            Used by {info?.guildCount} Servers
-                        </code>
-                    </Row>
+            <div className="p-0 m-0 container-fluid w-100 flex-wrap d-flex justify-content-center align-content-center fs-4 overflow-x-hidden mt-2">
+                <Row className="d-flex justify-content-center">
                     <Row className="d-flex justify-content-center">
                         <Carousel
                             keyboard={true}
@@ -32,7 +27,7 @@ export default function Home() {
                             className="carousel-dark"
                             style={{
                                 width: '50%',
-                                minWidth: '310px',
+                                minWidth: '315px',
                                 borderRadius: '5px',
                                 overflow: 'hidden',
                             }}
@@ -68,24 +63,30 @@ export default function Home() {
                         </Carousel>
                     </Row>
                 </Row>
-                <Row className="fs-4 text-center" style={{ minWidth: '310px', width: '75%' }}>
+                <Row className="d-flex justify-content-center mt-1 mb-1 w-100">
+                    <code className="m-1" style={{ width: 'fit-content', height: 'fit-content' }}>
+                        Used by {info?.guildCount} Servers
+                    </code>
+                </Row>
+                <Row className="text-center" style={{ minWidth: '315px', width: '75%' }}>
                     <p>
                         The Population Map Bot is a dynamic map generator that visualizes your Discord
                         server's population data on a global, continental, or country level. Maps are
                         generated from self-reported locations provided by server members and are anonymous.
                     </p>
                     <p>
-                        Server members use `/set-location` to set their country and, optionally, a subdivision
-                        (state, province, etc.) within that country. Maps are generated through the website,
-                        and you can get a link to a server's map with `/map`. Server admins can also make it
-                        so only members who share their location can access the server.{' '}
+                        Server members use <code>/set-location</code> to set their country and, optionally, a
+                        subdivision (state, province, etc.) within that country. Maps are generated through
+                        the website, and you can get a link to a server's map with <code>/map</code>. Server
+                        admins can also make it so only members who share their location can access the
+                        server.{' '}
                         <a
                             className="link"
                             href={
                                 'https://github.com/WeismanGitHub/Population-Map-Discord-Bot?tab=readme-ov-file#user-docs'
                             }
                         >
-                            Read more here.
+                            Read more here...
                         </a>
                     </p>
                 </Row>
